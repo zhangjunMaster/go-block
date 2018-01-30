504bb196417448c82f74c7548245335b0c48b85c
zhangjun <jun.zhang@yunshipei.com>
2018-01-30 18:52:22 +0800

    feature: 交易输入

--- block/block.go
+++ block/block.go
@@ -1,46 +1,49 @@
 package block
 
 import (
-	"strconv"
 	"bytes"
 	"crypto/sha256"
-	"time"
 	"fmt"
-	"math/big"
 	"math"
+	"math/big"
+	"strconv"
+	"time"
 	//"go-block/models"
 	// "github.com/satori/go.uuid"
+	. "go-block/transaction"
 )
 
 //生成区块
 type Block struct {
-    Timestamp     int64
-	Data          []byte
-	
-    PrevBlockHash []byte
+	Timestamp int64
+	//Data          []byte
+	Transactions  []*Transaction
+	PrevBlockHash []byte
 	Hash          []byte
-	Nonce 		  int
+	Nonce         int
 }
+
 //bytes.join
 //因为有了工作量证明，就不能随便塞hash了
-func NewBlock(data string, prevBlockHash []byte) *Block {
-	block := &Block{time.Now().Unix(), []byte(data), prevBlockHash, []byte{}, 0}
+func NewBlock(transactions []*Transaction, prevBlockHash []byte) *Block {
+	block := &Block{time.Now().Unix(), transactions, prevBlockHash, []byte{}, 0}
 	//[]byte{}空的byte类型的数组
 	pow := NewProofOfWork(block)
 	nonce, hash := pow.Run()
 	block.Hash = hash[:]
 	block.Nonce = nonce
-    return block
+	return block
 }
 
 //生成区块链
 type Blockchain struct {
-    Blocks []*Block
+	Blocks []*Block
 }
-//区块链就是区块的数组
-func (bc *Blockchain) AddBlock(data string) {
-    prevBlock := bc.Blocks[len(bc.Blocks)-1]
-    newBlock := NewBlock(data, prevBlock.Hash)
+
+//区块链就是区块的数组,将所有的data替换成transactions交易
+func (bc *Blockchain) AddBlock(transactions []*Transaction) {
+	prevBlock := bc.Blocks[len(bc.Blocks)-1]
+	newBlock := NewBlock(transactions, prevBlock.Hash)
 	bc.Blocks = append(bc.Blocks, newBlock)
 	//result, err := models.Db.Exec(
 	//	"INSERT INTO `block`(`name`,`password`) VALUES('tom', 'tom')"
@@ -48,13 +51,16 @@ func (bc *Blockchain) AddBlock(data string) {
 }
 
 //生成初创快
-func NewGenesisBlock() *Block {
-	return NewBlock("Genesis Block", []byte{}) 
+func NewGenesisBlock(coinbase *Transaction) *Block {
+	return NewBlock([]*Transaction{coinbase}, []byte{})
 }
 
-//生成区块链，*Block{}类型 NewGenesisBlock()生成的数据
-func NewBlockchain() *Blockchain {
-    return &Blockchain{[]*Block{NewGenesisBlock()}}
+//生成区块链，*Block{}类型 NewGenesisBlock()生成的数据，区块链必须有地址
+func NewBlockchain(address string) *Blockchain {
+	genesisCoinbaseData := "这是第一个块"
+	cbtx := NewCoinbaseTX(address, genesisCoinbaseData)
+	genesis := NewGenesisBlock(cbtx)
+	return &Blockchain{[]*Block{genesis}}
 }
 
 //工作量认证
@@ -62,13 +68,25 @@ const targetBits = 24
 
 //工作量认证的结构体
 type ProofOfWork struct {
-	block *Block
+	block  *Block
 	target *big.Int
+}
+
+func (b *Block) HashTransactions() []byte {
+	var txHashes [][]byte
+	var txHash [32]byte
+	// 获取，每一个交易的hash
+	for _, tx := range b.Transactions {
+		txHashes = append(txHashes, tx.ID)
+	}
+	// 最后获得一个连接后的组合哈希
+	txHash = sha256.Sum256(bytes.Join(txHashes, []byte{}))
+	return txHash[:]
 }
 
 //生成工作量认证
 func NewProofOfWork(b *Block) *ProofOfWork {
- 	target := big.NewInt(1)
+	target := big.NewInt(1)
 	target.Lsh(target, uint(256-targetBits))
 	pow := &ProofOfWork{b, target}
 	return pow
@@ -76,17 +94,17 @@ func NewProofOfWork(b *Block) *ProofOfWork {
 
 //准备数据进行hash
 func (pow *ProofOfWork) prepareData(nonce int) []byte {
-    data := bytes.Join(
-        [][]byte{
-            pow.block.PrevBlockHash,
-			pow.block.Data,
+	data := bytes.Join(
+		[][]byte{
+			pow.block.PrevBlockHash,
+			pow.block.HashTransactions(),
 			[]byte(strconv.FormatInt(pow.block.Timestamp, 16)),
 			[]byte(strconv.FormatInt(int64(targetBits), 16)),
 			[]byte(strconv.FormatInt(int64(nonce), 16)),
-        },
-        []byte{},
-    )
-    return data
+		},
+		[]byte{},
+	)
+	return data
 }
 
 //实现 pow算法
@@ -96,24 +114,24 @@ func (pow *ProofOfWork) Run() (int, []byte) {
 	var resultHash [32]byte
 	nonce := 0
 	maxNonce := math.MaxInt64
-	fmt.Printf("\nMining the block containing \"%s\"\n", pow.block.Data)
+	//fmt.Printf("\nMining the block containing \"%s\"\n", pow.block.Data)
 	for nonce < maxNonce {
 		data := pow.prepareData(nonce)
-		hash := sha256.Sum256(data)	
-		hashInt.SetBytes(hash[:]) //将哈希转换成一个大整数
+		hash := sha256.Sum256(data)
+		hashInt.SetBytes(hash[:])          //将哈希转换成一个大整数
 		if hashInt.Cmp(pow.target) == -1 { //将这个大整数与目标进行比较
 			fmt.Printf("\r%x", hash)
 			resultHash = hash
 			break
 		} else {
-            nonce++
-        }
+			nonce++
+		}
 	}
-    return nonce, resultHash[:]
+	return nonce, resultHash[:]
 }
 
 //检验工作量证明
-func (pow *ProofOfWork) Validate() bool{
+func (pow *ProofOfWork) Validate() bool {
 	var hashInt big.Int
 	data := pow.prepareData(pow.block.Nonce)
 	hash := sha256.Sum256(data)
@@ -121,4 +139,11 @@ func (pow *ProofOfWork) Validate() bool{
 	isValid := hashInt.Cmp(pow.target) == -1
 	return isValid
 }
- 
+
+/*
+func NewUTXOTransaction(from, to string, amount int, bc *Blockchain) *Transaction {
+	var inputs []TXInput
+	var outputs []TXOutput
+	acc, validOutputs := bc.FindSpendableOutputs(from, amount)
+}
+*/
--- transaction/transaction.go
+++ transaction/transaction.go
@@ -1,42 +1,39 @@
-package transaction;
+package transaction
 
 import (
-    "fmt"
-    "github.com/satori/go.uuid"
+	"fmt"
+
+	"github.com/satori/go.uuid"
 )
 
 //Vout是该输出在那笔交易中所有输出的索引
 //ScriptSig 是一个脚本，signature提供了可解锁输出结构里面 ScriptPubKey 字段的数据
 type TXInput struct {
-    Txid      []byte
-    Vout      int
-    ScriptSig string
-}
-//输出，value是一定量的币，ScriptPubKey是解锁的公钥
-type TXOutput struct {
-    Value        int
-    ScriptPubKey string
+	Txid      []byte
+	Vout      int
+	ScriptSig string
 }
 
 type Transaction struct {
-	ID []byte
-	Vin []TXInput
+	ID   []byte
+	Vin  []TXInput
 	Vout []TXOutput
 }
 
 func (self *Transaction) SetID() {
-    u1 := uuid.Must(uuid.NewV4()).String()
-    self.ID =  []byte(u1)
+	u1 := uuid.Must(uuid.NewV4()).String()
+	self.ID = []byte(u1)
 }
+
 //coinbase交易，这是第一次交易
 func NewCoinbaseTX(to, data string) *Transaction {
-    if data == "" {
-        data = fmt.Sprintf("Reward to '%s'", to)
-    }
-    subsidy := 50  //挖矿奖励  
-    txin := TXInput{[]byte{}, -1, data}
-    txout := TXOutput{subsidy, to}
-    tx := Transaction{nil, []TXInput{txin}, []TXOutput{txout}}
-    tx.SetID()
-    return &tx
+	if data == "" {
+		data = fmt.Sprintf("Reward to '%s'", to)
+	}
+	subsidy := 50 //挖矿奖励
+	txin := TXInput{[]byte{}, -1, data}
+	txout := TXOutput{subsidy, to}
+	tx := Transaction{nil, []TXInput{txin}, []TXOutput{txout}}
+	tx.SetID()
+	return &tx
 }
